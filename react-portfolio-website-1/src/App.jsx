68441a99f491875c561838d1e1afcf527f4c824d
tjef <102733963+tjef@users.noreply.github.com>
2022-04-15 23:32:54 -0500

    Added Header

--- react-portfolio-website-1/src/App.jsx
+++ react-portfolio-website-1/src/App.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import Header from './components/header/header';
+import Nav from './components/nav/nav';
+import About from './components/about/about';
+import Experience from './components/experience/experience';
+import Services from './components/services/services';
+import Portfolio from './components/portfolio/portfolio';
+import Testimonials from './components/testimonials/testimonials';
+import Contact from './components/contact/contact';
+import Footer from './components/footer/footer';
+
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+
+
+
+
+
+ 
+    </>
   )
 }
 
